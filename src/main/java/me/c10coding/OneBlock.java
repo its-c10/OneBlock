24969415090fc022599112a540e1212c9f2e9ae3
Caleb Owens <caleb.ja.owens@gmail.com>
2020-04-18 15:16:07 -0400

    Initial commit

---
+++ src/main/java/me/c10coding/OneBlock.java
@@ -0,0 +1,17 @@
+package me.c10coding;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OneBlock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
